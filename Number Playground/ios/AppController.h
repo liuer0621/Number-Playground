1071c4f92cecca44dd9a23a8db701dd222bb3dc9
Lana Sun <lanasun0621@Lanas-MacBook-Pro.local>
2013-06-09 17:45:13 +0800

    Initial Commit

---
+++ Number Playground/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  Number_PlaygroundAppController.h
+//  Number Playground
+//
+//  Created by Lana Sun on 6/9/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ Number Playground/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  Number_PlaygroundAppController.h
+//  Number Playground
+//
+//  Created by Lana Sun on 6/9/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
